2a8dace6b69d922571462185463254f1cfa32375
vineeth <vineeth@iist>
2017-03-23 16:43:40 +0530

    Initial code for configuration class for MUTTS GUI - brings all configuration related activities under a class

---
+++ GUI/MUTTS_Configuration.m
@@ -0,0 +1,74 @@
+classdef MUTTS_Configuration
+    properties
+        default_filename;
+        
+        dt;
+        dimension_observations;
+        use_additional_information;
+        observation_snr_limit;
+        observation_pointing_limit;
+        
+        % parameters for the input file
+        inputfile_parameters;
+        
+        % filter parameters
+        filter_type;
+        filter_parameters;
+        
+        % gating parameters
+        gating_method_type;
+        gating_method_parameters;
+        
+        % GNN parameters
+        data_association_type;
+        data_association_parameters;
+        
+        % Track maintenance parameters
+        track_maintenance_type;
+        track_maintenance_parameters;
+        
+        % Visualization parameters
+        visualization_parameters;
+        
+        % Metrics parameters
+        metrics_parameters;
+        
+        % Post processing parameters
+        atleastN_parameters;
+        velocitythreshold_parameters;
+    end
+    
+    methods
+        function o = MUTTS_Configuration(default_filename)
+            o.default_filename = default_filename;
+        end
+        
+        function o = load_default_configuration(o)
+            if exist(o.default_filename, 'file') == 2
+                run(o.default_filename); % this will populate the local workspace with the configuration variables that we need
+            else
+                error('%s does not exist!', o.default_filename);
+                return;
+            end
+            o.dt = dt;
+            o.dimension_observations = dimension_observations;
+            o.use_additional_information = use_additional_information;
+            o.observation_snr_limit = observation_snr_limit;
+            o.observation_pointing_limit = observation_pointing_limit;
+            o.inputfile_parameters = inputfile_parameters;
+            o.filter_type = filter_type;
+            o.filter_parameters = filter_parameters;
+            o.gating_method_type = gating_method_type;
+            o.gating_method_parameters = gating_method_parameters;
+            o.data_association_type = data_association_type;
+            o.data_association_parameters = data_association_parameters;
+            o.track_maintenance_type = track_maintenance_type;
+            o.track_maintenance_parameters = track_maintenance_parameters;
+            o.visualization_parameters = visualization_parameters;
+            o.metrics_parameters = metrics_parameters;
+            o.atleastN_parameters = atleastN_parameters;
+            o.velocitythreshold_parameters = velocitythreshold_parameters;
+        end
+    end    
+end
+
